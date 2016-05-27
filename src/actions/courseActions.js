878303f163188c1719184454295085097a090c14
Wlainer <wlainer@gmail.com>
2016-05-26 23:39:21 -0300

    Async Writes in Redux

--- src/actions/courseActions.js
+++ src/actions/courseActions.js
@@ -1,17 +1,35 @@
 import * as types from './actionTypes';
 import courseApi from '../api/mockCourseApi';
 
-export function createCourse(course) {
-  return {
-    type: types.CREATE_COURSE,
-    course
-  };
-}
-
 export function loadCoursesSuccess(courses) {
   return {
     type: types.LOAD_COURSES_SUCCESS,
     courses
+  };
+}
+
+export function createCourseSuccess(course) {
+  return {
+    type: types.CREATE_COURSE_SUCCESS,
+    course
+  };
+}
+
+export function updateCourseSuccess(course) {
+  return {
+    type: types.UPDATE_COURSE_SUCCESS,
+    course
+  };
+}
+
+export function saveCourse(course) {
+  return function(dispatch, getState) {
+    return courseApi.saveCourse(course).then(course => {
+      course.id ? dispatch(updateCourseSuccess(course)) :
+      dispatch(createCourseSuccess(course));
+    }).catch(error => {
+      throw(error);
+    });
   };
 }
 
--- src/components/course/CoursesPage.js
+++ src/components/course/CoursesPage.js
@@ -3,31 +3,20 @@ import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
+import {browserHistory} from 'react-router';
 
 class CoursesPage extends React.Component {
   constructor(props, context) {
     super(props, context);
-
-    this.state = {
-      course: { title: "" }
-    };
-
-    this.onTitleChange = this.onTitleChange.bind(this);
-    this.onClickSave = this.onClickSave.bind(this);
-  }
-
-  onTitleChange(event) {
-    const course = this.state.course;
-    course.title = event.target.value;
-    this.setState({course: course});
-  }
-
-  onClickSave() {
-    this.props.actions.createCourse(this.state.course);
+    this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
   }
 
   courseRow(course, index) {
     return <div key={index}>{course.title}</div>;
+  }
+
+  redirectToAddCoursePage() {
+    browserHistory.push('/course');
   }
 
   render() {
@@ -35,15 +24,11 @@ class CoursesPage extends React.Component {
     return(
       <div>
         <h1>Courses</h1>
-        <CourseList courses={courses} />
-        <h2>Add Course</h2>
-        <input
-          type="text"
-          onChange={this.onTitleChange}
-          value={this.state.course.title} />
         <input type="submit"
-          value="Save"
-          onClick={this.onClickSave} />
+                value="Add Course"
+                className="btn btn-primary"
+                onClick={this.redirectToAddCoursePage} />
+        <CourseList courses={courses} />
       </div>
     );
   }
